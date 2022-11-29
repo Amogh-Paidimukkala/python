50e87036f313e0e8f71fb2fe699e876edb8f6da5
Amogh-Paidimukkala <113460619+Amogh-Paidimukkala@users.noreply.github.com>
2022-11-29 16:29:07 -0400

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,3 +1,8 @@
+#Collatz Conjecture
+
+#Choose a number greater than zero, if it is an even number divide it by 2 else multuply the number with 3 and add 1
+#The result forms an endless loop after few iterations. So terminate it when the value reaches 1.
+
 a=int(input("Enter a number greater than 0"));
 print("You chose number ",a);
 while(a>1):
